18853db5d40465ff1f5d80f1e16c2cb6dbf4f95f
CarlesRojas <carles.rojas@mundimoto.com>
2022-11-18 11:02:23 +0100

    add document page to all

---
+++ styled-components/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="application-name" content="CSS Modules" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="CSS Modules" />
+        <meta name="description" content="CSS Modules" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
